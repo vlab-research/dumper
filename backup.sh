f8a95fd7cebf2d2a246a4eea5471dcdfa553a570
Nandan Rao <nandanmarkrao@gmail.com>
2021-02-13 10:22:28 +0100

    added 6 day cleanup, keep only one per day

--- backup.sh
+++ backup.sh
@@ -10,4 +10,8 @@ cockroach dump chatroach --insecure --host $CHATBASE_HOST > "$TIMESTAMP".sql
 
 gsutil cp "$TIMESTAMP".sql $DUMPER_LOCATION
 
-echo "BACKUP FINISHED COPYING FILE AT $TIMESTAMP!"
+echo "BACKUP COPIED FILE AT $TIMESTAMP."
+
+gsutil ls $DUMPER_LOCATION | grep "$(date --date '-6 day' --iso-8601)" | tail -n +2 | gsutil rm -I
+
+echo "BACKUP FINISHED."
